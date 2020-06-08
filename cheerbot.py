ed0d20f44cce42de4b555d266a667a5c37916f20
Melody <leclecticat@gmail.com>
2020-06-08 16:49:41 -0700

    adding first bot command

--- cheerbot.py
+++ cheerbot.py
@@ -1,15 +1,36 @@
-mport os
-
+import os
+import random
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+#client = discord.Client()
+bot = commands.Bot(command_prefix='^')
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+@bot.command(name='getQuote')
+async def quotes(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
 
-client.run(TOKEN)
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice, number_of_sides):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+bot.run(TOKEN)
